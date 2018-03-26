3df1524d325b27cd5d4b8f7b30c7a9fd89f4874b
haohaohihi <haohaohihi@gmail.com>
2018-03-26 17:05:53 +0800

    完善程序，增加部分测试图片

--- SiftDetector.py
+++ SiftDetector.py
@@ -1,12 +1,14 @@
+# -*- coding: UTF-8 -*-
 import os
+import sys
 import cv2
 import time
-import pickle
+import numpy as np
 
 # 参考了https://docs.opencv.org/3.0-beta/doc/py_tutorials/py_feature2d/py_matcher/py_matcher.html#matcher
 # 使用了传统的SIFT进行特征值提取,FLANN进行匹配
 
-MIN_MATCH_COUNT = 20 
+MIN_MATCH_COUNT = 10 
 FLANN_INDEX_KDTREE = 0
 index_params = dict(algorithm = FLANN_INDEX_KDTREE, trees = 5)
 search_params = dict(checks = 50)
@@ -21,16 +23,18 @@ class SiftDetector:
         self.__create_result_folder()
     
     def create_database(self):
+        """
+        读取模板目录里的所有图片，并计算sift特征值，存储在内存中供后续匹配
+        """
         print("初始化，创建匹配数据库中...")
         database = dict()
         for t_path in os.listdir(self.templates_path):
-
             t_full_path = os.path.join(self.templates_path, t_path)
             img = cv2.imread(t_full_path, 1)
             try:
                 kp, des = self.sift.detectAndCompute(img, None)
-            except:
-                print(t_path)
+            except Exception as e:
+                print(e)
             classname = t_path.split('.')[0]
             database[classname] = {
                                     'd_kp': kp,
@@ -41,6 +45,9 @@ class SiftDetector:
         return database
     
     def match_one_image(self, test_img_detail):
+        """
+        将读取并计算了sift特征值的图片，对模板库的图片进行特征匹配
+        """
         start_time = time.time()
         best_matches = []
         best_classname = ""
@@ -76,55 +83,12 @@ class SiftDetector:
             print("匹配成功，结果为: %s" % best_classname)
             print("花费时间：%s" % (time.time() - start_time))
             print("----------------------------------------------------")
-            return True, best_classname, len(best_matches)
-
-    def match_one_image_1(self, test_img_detail):
-        start_time = time.time()
-        match_dict = {}
-
-        for k, v in self.database.items():
-            match_dict[k] = []
-            try:
-                matches = flann.knnMatch(v.get('d_des'), test_img_detail.get('t_des'), k=2)
-            except:
-                raise CannotMatchException
-            # get good matches
-            good = []
-            for m, n in matches:
-                if m.distance < 0.75*n.distance:
-                    good.append(m)
-            match_dict[k].append(len(good))
-
-        max_count = 0
-        maybe_name = ""
-        for k, v in match_dict.items():
-            count_temp = sum(v) / len(v)
-            if count_temp > max_count:
-                max_count = count_temp
-                maybe_name = k
-                
-        if max_count > MIN_MATCH_COUNT:
-            best_classname = maybe_name
-        else:
-            best_classname = ""
-
-        if not best_classname:
-            print("测试文件为: %s" % test_img_detail.get('t_image_name'))
-            print("未匹配成功，最终匹配特征点数 - %d/%d" % (max_count, MIN_MATCH_COUNT))
-            print("结果可能为 %s" % maybe_name)
-            print("花费时间：%s" % (time.time() - start_time))
-            print("----------------------------------------------------")
-            return False, maybe_name, max_count 
-        else:
-            print("测试文件为: %s" % test_img_detail.get('t_image_name'))
-            print("最终匹配特征点数为: %d" % max_count)
-            print("匹配成功，结果为: %s" % best_classname)
-            print("花费时间：%s" % (time.time() - start_time))
-            print("----------------------------------------------------")
-            return True, best_classname, max_count
-
+            return True, best_classname, len(best_matches), best_matches
 
     def draw_or_save_match_result(self, test_img_detail, best_classname, best_matches):
+        """
+        将满足阈值的匹配结果进行对比绘制
+        """
         if not best_matches:
             return None
 
@@ -158,4 +122,30 @@ class SiftDetector:
         if self.result_path and  not os.path.exists(self.result_path):
             os.mkdir(self.result_path)
 
+
+def readImg(img_path):
+    """
+    从路径中读取一个图片文件并计算对应的特征值
+    """
+    img = cv2.imread(img_path, 1)
+    sift = cv2.xfeatures2d.SIFT_create()
+    t_kp, t_des = sift.detectAndCompute(img,None)
+    img_detail = {
+                    't_image_name': img_path,
+                    't_img': img,
+                    't_kp': t_kp,
+                    't_des': t_des,
+    }
+    return img_detail
+
+def main():
+    img_path = sys.argv[1]
+    siftDetector = SiftDetector("templates", "result")
+    img = readImg(img_path)
+    result = siftDetector.match_one_image(img)
+    if result[0] is True:
+        siftDetector.draw_or_save_match_result(img, result[1], result[-1])
+
+if __name__ == "__main__":
     
+    main()
